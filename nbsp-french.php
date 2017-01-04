3d65e631afbd59646183362b10706e0802be997d
JS Morisset <jsm@surniaulula.com>
2017-01-04 13:19:26 -0800

    v1.8.1-1

--- nbsp-french.php
+++ nbsp-french.php
@@ -33,7 +33,7 @@
  * PARTICULAR PURPOSE. See the GNU General Public License for more details at
  * http://www.gnu.org/licenses/.
  * 
- * Copyright 2012-2016 Jean-Sebastien Morisset (https://surniaulula.com/)
+ * Copyright 2012-2017 Jean-Sebastien Morisset (https://surniaulula.com/)
  */
 
 if ( ! defined( 'ABSPATH' ) ) 
