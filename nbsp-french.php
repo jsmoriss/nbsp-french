eb13a58358a3b71a618530096e4101e99acc5fa1
JS Morisset <jsm@surniaulula.com>
2017-03-06 18:40:00 -0800

    v1.8.1-1

--- nbsp-french.php
+++ nbsp-french.php
@@ -11,7 +11,7 @@
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Adds a non-breaking space between words and punctuation marks to avoid inappropriate line-breaks in French.
  * Requires At Least: 3.8
- * Tested Up To: 4.7.2
+ * Tested Up To: 4.7.3
  * Version: 1.8.1-1
  *
  * Version Components: {major}.{minor}.{bugfix}-{stage}{level}
