1ffcb432e0286e9032967a8543a1138cdd450bc3
JS Morisset <jsm@surniaulula.com>
2020-06-11 08:55:22 -0700

    v1.10.0

--- nbsp-french.php
+++ nbsp-french.php
@@ -12,7 +12,7 @@
  * Description: Adds a non-breaking space between words and punctuation marks to avoid inappropriate line-breaks in French.
  * Requires PHP: 5.6
  * Requires At Least: 4.2
- * Tested Up To: 5.4.1
+ * Tested Up To: 5.4.2
  * Version: 1.10.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
